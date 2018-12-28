68ba2c2daee6a9d3cce60400f8ee9dcc6d75ada0
Pedro Renato Ricardo Ramos <pedro.ramos1@ibm.com>
2018-12-28 20:38:48 -0200

    add play again

--- tictactoe.py
+++ tictactoe.py
@@ -1,76 +1,114 @@
 # coding: utf8
 
 
-class Game:
-    pos = ["\n", 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    jogador = "X"
-    isGameOver = False
+def printBoard():  # Exibe o tabuleiro no terminal;
+    print("")
+    print("  {} | {} | {} ".format(board[1], board[2], board[3]))
+    print(" ---+---+--- ")
+    print("  {} | {} | {} ".format(board[4], board[5], board[6]))
+    print(" ---+---+--- ")
+    print("  {} | {} | {} ".format(board[7], board[8], board[9]))
+    print("")
 
-    def main(self):
-        while self.isGameOver == False:
-            self.board()        # Carrega e atualiza o tabuleiro;
-            self.jogada()       # Gerencia as jogadas;
-            self.gameOver()     # Verifica se o jogo acabou;
-            self.novaRodada()   # Prepara o proximo jogador;
 
-        print("Jogar Novamente? (S / N)", end=" ")
-        newgame = input()
+def isPosFree(pos):  # Verifica se a posicao selecionada esta disponivel;
+    return board[pos] in range(1, 10)
 
-        if newgame in ["S", "s"]:
-            self.isGameOver = False
-            self.main()
-        else:
+
+def isBoardFull(board):  # Verifica se o tabuleiro esta cheio;
+    countX = board.count("X")
+    countO = board.count("O")
+    countAll = countX + countO
+
+    if countAll > 8:
+        return True
+    else:
+        return False
+
+
+def placeLetter(letter, pos):  # Insere a letra no tabuleiro;
+    board[pos] = letter
+
+
+def playerMove(letter):  # Gerencia cada jogada;
+    while True:
+        move = input("Selecione uma posição para jogar (1-9): ")
+
+        if move == "q":
             quit()
 
-    def board(self):
-        board = "{1}Jogador: {0} {1}".format(self.jogador, self.pos[0])
-        board += "    {} | {} | {} {}".format(
-            self.pos[1], self.pos[2], self.pos[3], self.pos[0])
-        board += "   ---+---+--- {}".format(self.pos[0])
-        board += "    {} | {} | {} {}".format(
-            self.pos[4], self.pos[5], self.pos[6], self.pos[0])
-        board += "   ---+---+--- {}".format(self.pos[0])
-        board += "    {} | {} | {} {}".format(
-            self.pos[7], self.pos[8], self.pos[9], self.pos[0])
-        print(board)
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if isPosFree(move):
+                    placeLetter(letter, move)
+                    break
+                else:
+                    print(">>> Espaço ocupado !!! \n")
+            else:
+                print(">>> Posição inválida! \n")
+        except:
+            print(">>> Favor informar um número! \n")
 
-    def jogada(self):
-        jogada = input("Proxima jogada: ")
 
-        novaJogada = str(self.pos[int(jogada)])
+def checkWinner(letter):  # Verifica se houve um vencedor;
+    isGameOver = False
 
-        if novaJogada == "X" or novaJogada == "O":
-            self.main()
+    if (board[1] == letter and board[2] == letter and board[3] == letter):
+        isGameOver = True
+    elif (board[4] == letter and board[5] == letter and board[6] == letter):
+        isGameOver = True
+    elif (board[7] == letter and board[8] == letter and board[9] == letter):
+        isGameOver = True
+    elif (board[1] == letter and board[4] == letter and board[7] == letter):
+        isGameOver = True
+    elif (board[2] == letter and board[5] == letter and board[8] == letter):
+        isGameOver = True
+    elif (board[3] == letter and board[6] == letter and board[9] == letter):
+        isGameOver = True
+    elif (board[1] == letter and board[5] == letter and board[9] == letter):
+        isGameOver = True
+    elif (board[3] == letter and board[5] == letter and board[7] == letter):
+        isGameOver = True
+
+    return isGameOver
+
+
+def getLetter(count):  # Gerencia o jogador atual;
+    if count % 2 == 0:
+        letter = "X"
+    else:
+        letter = "O"
+
+    return letter
+
+
+def main():  # main
+    print("\n     --- TIC TAC TOE ---     ")
+    printBoard()
+
+    global board
+    count = 0
+
+    while True:
+        if isBoardFull(board):
+            print(">>> Empate! \n")
+            break
         else:
-            self.pos[int(jogada)] = self.jogador
+            letter = getLetter(count)
+            playerMove(letter)
+            printBoard()
 
-    def novaRodada(self):
-        if self.jogador == "X":
-            self.jogador = "O"
-        else:
-            self.jogador = "X"
-
-    def gameOver(self):
-        gameover = ""
-
-        if all(a == self.jogador for a in self.pos[1:4]):
-            gameover = "* O JOGADOR {} VENCEU! *".format(self.jogador)
-            self.isGameOver = True
-        elif all(a == self.jogador for a in self.pos[4:7]):
-            gameover = "* O JOGADOR {} VENCEU! *".format(self.jogador)
-            self.isGameOver = True
-        elif all(a == self.jogador for a in self.pos[7:10]):
-            gameover = "* O JOGADOR {} VENCEU! *".format(self.jogador)
-            self.isGameOver = True
-
-        print(gameover)
+            if not(checkWinner(letter)):
+                count += 1
+            else:
+                print(">>> O jogador {} venceu! \n".format(letter))
+                break
 
 
-game = Game()
-game.main()
-
-"""
-to-do
-logica do jogo na classe gameover, dizendo jogador vencedor ou VELHA
-opcao para jogar novamente ao fim de uma partida
-"""
+while True:
+    board = [x for x in range(0, 10)]
+    main()
+    newgame = input("Jogar Novamente? (S / N): ")
+    if newgame in ["N", "n", "q"]:
+        quit()
